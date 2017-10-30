1dbdf4a3904577672d68a1cfcf2151c9652c224f
Marko Savic <Marko.Savic@zuhlke.com>
2017-10-30 21:40:42 +0100

    fix super estimator view

--- src/main/java/com/marko/model/context/EstimationContext.java
+++ src/main/java/com/marko/model/context/EstimationContext.java
@@ -4,6 +4,16 @@ package com.marko.model.context;
  * Created by msav on 10/30/2017.
  */
 public enum EstimationContext {
-    REFINEMENT,
-    REVEALMENT
+    REFINEMENT("refinement"),
+    REVEALMENT("revealment");
+
+    private final String contextName;
+
+    EstimationContext(String contextName) {
+        this.contextName = contextName;
+    }
+
+    public String getContextName() {
+        return contextName;
+    }
 }
